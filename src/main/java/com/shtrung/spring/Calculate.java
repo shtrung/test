01e62f56daa819177ac8a7120b4c2ad4e658c799
simon <shtrung.se@yandex.ru>
2025-05-01 23:52:29 +0700

    Update

--- src/main/java/com/shtrung/spring/Calculate.java
+++ src/main/java/com/shtrung/spring/Calculate.java
@@ -2,7 +2,13 @@ package com.shtrung.spring;
 
 public class Calculate {
 
-    public int add (int x, int y){return x+y +223;}
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
 
 
 }
